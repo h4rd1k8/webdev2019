cbbdeea1521ee1cf610741965d7c39fbab833cf5
Yerdaulet <35425173+h4rd1k8@users.noreply.github.com>
2019-02-03 13:55:17 +0600

    msg

---
+++ week0/js_codeacademy/1.js
@@ -0,0 +1,13 @@
+console.log('Hello' + 'World');
+console.log('Hello' + ' ' + 'World');
+console.log('Teaching the world how to code'.length);
+
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+console.log(Math.floor(Math.random()*100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
